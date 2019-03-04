75a232a13b375c3b570003a467bed52f722898b4
simon.shimizu <simon80696@gmail.com>
2019-03-04 11:35:11 +0000

    have

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  validates :code, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :url, presence: true, length: { maximum: 255 }
+  validates :image_url, presence: true, length: { maximum: 255 }
+
+  has_many :wants
+  has_many :want_users, through: :wants, source: :user
+  
+  has_many :has
+  has_many :have_users, through: :has, source: :user
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root to: 'toppages#index'
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
+
+  resources :items, only: [:show, :new]
+  resources :ownerships, only: [:create, :destroy]
+end
